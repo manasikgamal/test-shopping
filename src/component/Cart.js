ab0288f71e3c79c5da5666ae5e9e6359c92b68dd
manasikgamal <manasikgamal44@gmail.com>
2021-11-08 04:54:59 -0800

    cart2

---
+++ src/component/Cart.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Cartgallary from "./Cartgallary";
+import Price from './Price';
+export default class Cart extends Component {
+
+    render() {
+        const {cartitems}=this.props;
+        return (
+            <div className="carcontainert">
+                 <div className={this.props.isVisible?"cartbtnshadow":""}></div>
+                <div className="cardc">
+                CART
+                </div>
+                
+                    <ul>
+                        {cartitems.map((item)=>(
+                          <li key={item.id}> 
+                              <div>
+                    <div className={this.props.isVisible?"cartdet container":"cartdet"} >
+                    <hr/>
+                                 <div className="cardtitle">
+                                    {item.name}
+                                </div>
+                                <div className="brand">
+                                {item.brand}
+                                </div>
+                                <div className="cardprice">
+                                <Price currn={this.props.currn} price={item.prices}/>
+                                </div>
+                                <div>
+                     {item.items.map((pro,index)=>(
+                    <div className="sizeshow" key={index}>
+                     <li><button style={{backgroundColor:pro.type==="swatch"?pro.choose:"",
+                     color:pro.type==="swatch"?pro.choose:"",
+                     border:pro.type==="swatch"?"1px solid black":"",
+                     }}>{pro.choose}</button></li>
+                    </div>))}
+                                </div>
+                                </div>
+                                <div className={this.props.isVisible?"maxmin container":"maxmin"}>
+                                <hr/>
+                                <button onClick={()=>this.props.addtocart(item)} className="max">+</button>
+                                <p>{item.count}</p>
+                                    <button onClick={()=>this.props.removefromcard(item)} className="min">-</button>
+                                </div>
+                               <li> <div className="cartimg">
+                               <hr/>
+                              <Cartgallary images={item.gallery} isVisible={this.props.isVisible}/>
+                                </div>
+                                  </li>
+                              </div>
+                               
+                          </li>
+                        ))}
+                    </ul>
+                </div>
+            
+        )
+    }
+}
